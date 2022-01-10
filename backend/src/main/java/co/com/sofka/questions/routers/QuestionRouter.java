2786bedd0795ce17dc012ba8ab9af6327b57b6d0
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2022-01-10 17:26:03 -0500

    modificando la ruta de la question [backend]

--- backend/src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
+++ backend/src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
@@ -1,7 +1,9 @@
 package co.com.sofka.questions.routers;
 
 import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.FavoriteDTO;
 import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.model.UpdateVoteDTO;
 import co.com.sofka.questions.usecases.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,11 +22,11 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class QuestionRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> getAll(ListUseCase listUseCase) {
+    public RouterFunction<ServerResponse> getAll(ListQuestionUseCase listQuestionUseCase) {
         return route(GET("/getAll"),
                 request -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
-                        .body(BodyInserters.fromPublisher(listUseCase.get(), QuestionDTO.class))
+                        .body(BodyInserters.fromPublisher(listQuestionUseCase.get(), QuestionDTO.class))
         );
     }
 
@@ -37,20 +39,32 @@ public class QuestionRouter {
                         .body(BodyInserters.fromPublisher(
                                 ownerListUseCase.apply(request.pathVariable("userId")),
                                 QuestionDTO.class
-                         ))
+                        ))
         );
     }
 
     @Bean
-    public RouterFunction<ServerResponse> create(CreateUseCase createUseCase) {
-        Function<QuestionDTO, Mono<ServerResponse>> executor = questionDTO ->  createUseCase.apply(questionDTO)
+    public RouterFunction<ServerResponse> create(CreateQuestionUseCase createQuestionUseCase) {
+        Function<QuestionDTO, Mono<ServerResponse>> executor = questionDTO -> createQuestionUseCase.apply(questionDTO)
                 .flatMap(result -> ServerResponse.ok()
                         .contentType(MediaType.TEXT_PLAIN)
                         .bodyValue(result));
-
         return route(
                 POST("/create").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(QuestionDTO.class).flatMap(executor)
+        );
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> updateVote(UpdateVotesUseCase updateVotesUseCase) {
+        Function<UpdateVoteDTO, Mono<ServerResponse>> executor = updateVoteDTO -> updateVotesUseCase.updateVotes(updateVoteDTO)
+                .flatMap(result -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(result));
+        return route(
+                PUT("/updatevote").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(UpdateVoteDTO.class).flatMap(executor)
         );
     }
 
@@ -61,8 +75,21 @@ public class QuestionRouter {
                 request -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(BodyInserters.fromPublisher(getUseCase.apply(
-                                request.pathVariable("id")),
+                                        request.pathVariable("id")),
                                 QuestionDTO.class
+                        ))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getFavorites(GetFavoritesUseCase getFavoritesUseCase) {
+        return route(
+                GET("/favorite/{id-user}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getFavoritesUseCase.apply(
+                                        request.pathVariable("id-user")),
+                                FavoriteDTO.class
                         ))
         );
     }
@@ -80,12 +107,41 @@ public class QuestionRouter {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> addFavorite(AddFavoriteUseCase addFavoriteUseCase) {
+            return route(POST("/favorite/add").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(FavoriteDTO.class)
+                        .flatMap(favoriteDTO -> addFavoriteUseCase.apply(favoriteDTO)
+                                .flatMap(result -> ServerResponse.ok()
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                        )
+        );
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> delete(DeleteUseCase deleteUseCase) {
         return route(
                 DELETE("/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse.accepted()
                         .contentType(MediaType.APPLICATION_JSON)
-                        .body(BodyInserters.fromPublisher(deleteUseCase.apply(request.pathVariable("id")), Void.class))
+                        .body(BodyInserters.fromPublisher(deleteUseCase.apply(request.pathVariable("id")),
+                                Void.class))
+        );
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> removeFavorite(RemoveFavoriteUseCase removeFavoriteUseCase) {
+        return route(
+                PUT("/favorite/remove").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(FavoriteDTO.class)
+                        .flatMap(favoriteDTO -> removeFavoriteUseCase.apply(favoriteDTO)
+                                .flatMap(result -> ServerResponse.ok()
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result)))
+                        .onErrorResume(
+                                throwable -> ServerResponse.badRequest().body(throwable.getMessage(),
+                                String.class))
         );
     }
 }
